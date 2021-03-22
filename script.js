b9851cb51d1e098c01f5f2e8355dc014fa150e73
orwa97 <orwa.aloufi@gmail.com>
2021-03-22 16:03:31 +0200

    copy to clipboaed+animated popup message

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ const eventLocationSpecifi = eventLocationParent.children[1];
 const eventWhich = document.querySelector(".event--which");
 const eventCode = document.querySelector(".event--code");
 const boxes = document.querySelector(".boxes");
+const boxesText = document.querySelectorAll(".box__display-feild");
+const copy1Notify = document.querySelector(".copied--1");
+const copy2Notify = document.querySelector(".copied--2");
+const headache = document.querySelector(".headache");
+const orwa = document.querySelector(".orwa");
 const keyLocations = [
   "General keys",
   "Left-side modifier keys",
@@ -39,12 +44,30 @@ key();
 
 //Copy the JavaScript event keycode to the clipboard
 // by clicking on the event box
-// for (let i = 0; i < boxes.children.length; i++) {
-//   boxes.children[i].addEventListener("click", function () {
-//     let copyText = boxes.children[i].textContent;
-//     console.log("clicked on box", copyText);
-//     document.execCommand("Copy");
-//   });
-// }
-//
-//
+for (let i = 0; i < boxes.children.length; i++) {
+  boxes.children[i].addEventListener("click", function () {
+    const el = document.createElement("textarea");
+    let str;
+    for (let e = 0; e < boxesText.length; e++) {
+      str = boxesText[i].textContent;
+      el.value = str;
+    }
+    console.log(str);
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+    //Copied to clipborad's functionality
+    if (!copy1Notify.classList.contains("clicked")) {
+      copy2Notify.classList.remove("clicked");
+      copy1Notify.classList.add("clicked");
+    } else {
+      copy1Notify.classList.remove("clicked");
+      copy2Notify.classList.add("clicked");
+    }
+  });
+}
+
+orwa.addEventListener("click", function () {
+  headache.classList.toggle("clickedOnOrwa");
+});
